ffbcdfdecfdd8b7fa9e26fab3c6d5498d9b635eb
mezencev <33099967+Mezencev@users.noreply.github.com>
2023-09-12 15:31:18 +0300

    Update unitedSurvey.js
    add code

--- unitedSurvey.js
+++ unitedSurvey.js
@@ -37,3 +37,11 @@ lpTag.hooks.push({
   name: "AFTER_GET_LINES",
   callback: lpTag.external.surveyBot.afterGetLines,
 });
+
+
+function hideWrapperLine() {
+  const line = document.getElementsByClassName("lp_time lp_chat_line_auto_message lpc_message-area__timestamp lpc_message-area__timestamp_auto lpc_desktop lp_underline");
+  console.log(line.length, "LINE");
+  //line[0].style.visibility = "hidden";
+  line[line.length - 1].style.visibility = "hidden";
+}
