116b81499385267c8aa10bb7a765812cab2f30b2
mezencev <33099967+Mezencev@users.noreply.github.com>
2023-09-15 14:38:31 +0300

    update logic
    update logic

--- unitedSurvey.js
+++ unitedSurvey.js
@@ -1,31 +1,26 @@
 lpTag.external = lpTag.external || {};
 const LAST_MESSAGE = "Please provide your confirmation or reference number, full name and a sentence summarizing the assistance you need.";
+var restartFlag = false;
 
 lpTag.external.surveyBot = {
   // handle incoming messages
   afterGetLines: function (data) {
     try {
       if (data.data.lines.length > 0) {
-        const restartFlag = localStorage.getItem("restartFlag");
-        if (data.data && data.data.lines && data.data.lines.length && data.data.lines[0].isWelcomeMessage && restartFlag == "true") {
+        if (data.data && data.data.lines && data.data.lines.length && data.data.lines[0].isWelcomeMessage && restartFlag) {
           console.log("new welcome message");
-          setTimeout(hideWrapperLine, 0);
+          setTimeout(hideWrapperLine, 100);
           console.log(`DATA: ${JSON.stringify(data.data.lines[0])}`);
           delete data.data.lines[0].quickReplies;
           delete data.data.lines[0].text;
+        } else if (data.data && data.data.lines && data.data.lines.length && data.data.lines[0].by == "Survey Bot" && data.data.lines[0].text.startsWith(LAST_MESSAGE)) {
+          console.log("survey");
+          restartFlag = true;
+        } else if (data.data && data.data.lines && data.data.lines.length && data.data.lines[0].text && data.data.lines[0].state == "SENT") {
+          //console.log("new message", data.data.lines[0]);
+          restartFlag = false;
         }
-        if (data.data && data.data.lines && data.data.lines.length && data.data.lines[0].by == "Survey Bot" && data.data.lines[0].text.startsWith(LAST_MESSAGE)) {
-          localStorage.setItem("restartFlag", true);
-          setTimeout(() => {
-            console.log("UPDATE FLAG");
-            localStorage.setItem("restartFlag", false);
-          }, 86400000); //86400000
-        }
-        if (data.data && data.data.lines && data.data.lines.length && data.data.lines[0].source == "agent" && restartFlag == "true" && data.data.lines[0].state == "ACCEPT") {
-          console.log("agent Accept conversation");
-          console.log(`DATA: ${JSON.stringify(data.data.lines[0])}`);
-          localStorage.setItem("restartFlag", false);
-        }
+        
       }
     } catch (e) {
       console.error(e);
